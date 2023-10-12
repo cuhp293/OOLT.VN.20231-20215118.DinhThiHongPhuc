4f1a5eda2b9e4da4ca307cd6d0d3ebb15209163c
cuhp293 <phuchong292003@gmail.com>
2023-10-13 01:24:24 +0700

    Complete assignment 2.2.5

---
+++ Lab1/CalculateDoubleNumber.java
@@ -0,0 +1,19 @@
+// Example 5: CalculateDoubleNumber.java
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class CalculateDoubleNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        double num1 = Double.parseDouble(a);
+        double num2 = Double.parseDouble(b);
+
+        System.out.println("Sum of 2 double numbers: " + (num1 + num2));
+        System.out.println("Difference of 2 double numbers: " + (num1 - num2));
+        System.out.println("Product of 2 double numbers: " + (num1 * num2));
+        System.out.println("Quotient of 2 double numbers: " + (num1 / num2));
+    }
+}
