df86c0b4cad638ddb4e96e9762ded1f498567dd3
cuhp293 <phuchong292003@gmail.com>
2023-12-18 16:17:51 +0700

    update Painter app

--- GUIProject/src/hust/soict/hedspi/javafx/PainterController.java
+++ GUIProject/src/hust/soict/hedspi/javafx/PainterController.java
@@ -2,6 +2,8 @@ package hust.soict.hedspi.javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -11,6 +13,28 @@ public class PainterController {
 
     @FXML
     private Pane drawingAreaPane;
+    
+    @FXML
+    private RadioButton drawingPen;
+    
+    @FXML
+    private RadioButton eraserPen;
+    
+    @FXML
+    private ToggleGroup tools;
+    
+    @FXML
+    void selected(ActionEvent event) {
+		ToggleGroup select = new ToggleGroup();	
+		drawingPen.setToggleGroup(select);
+		eraserPen.setToggleGroup(select);
+		if(drawingPen.isSelected()) {
+			eraserPen.setSelected(false);
+		}
+		else {
+			drawingPen.setSelected(false);
+		}
+    }
 
     @FXML
     void clearButtonPressed(ActionEvent event) {
@@ -19,9 +43,13 @@ public class PainterController {
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-    	Circle newCircle = new Circle(event.getX(),
-    			event.getY(), 4, Color.BLACK);
-    	drawingAreaPane.getChildren().add(newCircle);
+    	if(drawingPen.isSelected()) {
+			Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
+			drawingAreaPane.getChildren().add(newCircle);			
+		}
+		else {
+			Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.WHITE);
+			drawingAreaPane.getChildren().add(newCircle);	
+		}
     }
-
 }
